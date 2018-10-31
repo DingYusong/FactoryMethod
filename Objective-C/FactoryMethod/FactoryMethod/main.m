25a9341952ad98d6c901eeda1bb874ff02a8da07
丁玉松 <yusong.ding@huangjinqianbao.com>
2018-10-31 10:16:32 +0800

    first commit

---
+++ Objective-C/FactoryMethod/FactoryMethod/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  FactoryMethod
+//
+//  Created by 丁玉松 on 2018/10/30.
+//  Copyright © 2018 丁玉松. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/**
+ 学雷锋志愿者帮助孤寡老人
+ 
+ 学雷锋志愿者有学生和社区志愿者
+ 
+ 主要帮助老人做洗衣服，打扫卫生，买菜这些活。
+ 
+ */
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
